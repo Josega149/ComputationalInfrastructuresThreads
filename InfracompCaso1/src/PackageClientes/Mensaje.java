70da7266ab79fa68fb5517d1e9067e108b0e709b
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2016-09-10 15:54:40 -0500

    Clase mensaje

--- InfracompCaso1/src/PackageClientes/Mensaje.java
+++ InfracompCaso1/src/PackageClientes/Mensaje.java
@@ -1,5 +1,33 @@
 package PackageClientes;
 
 public class Mensaje {
-
+	
+	private int mensaje;
+	
+	public Mensaje (int msj)
+	{
+		mensaje = msj;
+	}
+	
+	public void modificarMensaje ()
+	{
+		mensaje++;
+	}
+	
+	public int darMensaje()
+	{
+		return mensaje;
+	}
+	
+	public void dormir()
+	{
+		try 
+		{
+			wait();
+		} 
+		catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
